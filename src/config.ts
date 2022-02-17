844da3ede6613a728c05ba8c985a6a1ccf2dbb55
FlyingHail <flyinghail@msn.com>
2022-02-18 00:20:17 +0800

    php change to apachelounge build

--- src/config.ts
+++ src/config.ts
@@ -31,7 +31,7 @@ export default {
             version: '8.1.2',
             config: 'php.ini',
             ignore: ['extras/'],
-            url: 'https://windows.php.net/downloads/releases/php-{version}-nts-Win32-vs16-x64.zip'
+            url: 'https://phpdev.toolsforresearch.com/php-{version}-nts-Win32-vs16-x64.zip'
         },
         {
             name: 'phpMyAdmin',
